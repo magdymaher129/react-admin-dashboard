37ca079f159d2b9e4502bd02488ac58a5e098885
magdy maher <magdymaher229@gmail.com>
2022-08-28 15:54:57 +0200

    repair footer

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap'
 const Footers = () => {
   return (
     <div>
-      <div style={{backgroundColor:"#3b3a3a",height:"60px",color:"#fff", marginTop:"40px"}}>
+      <div style={{backgroundColor:"#3b3a3a",height:"60px",color:"#fff", marginTop:"auto"}}>
     
       <Card.Body style={{borderRadius: '0px! important '}}>
        
